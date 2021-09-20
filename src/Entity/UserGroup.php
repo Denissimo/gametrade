29583ae1ccea7f47c5e39a8135dd3521ffdacf2a
Den <droden@narod.ru>
2021-09-20 17:48:10 +0300

    FOS

---
+++ src/Entity/UserGroup.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user_group")
+ */
+class UserGroup extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
